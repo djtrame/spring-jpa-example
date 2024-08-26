080c18728a17c410ea2a68fd2d21dbb20d716380
djtrame <king_bucket@hotmail.com>
2024-08-25 22:00:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring-jpa-example
+ Spring Boot Rest API with JPA examples such as a junction table
---
+++ src/main/java/com/trame/spring_jpa_example/SpringJpaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.trame.spring_jpa_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaExampleApplication.class, args);
+	}
+
+}
