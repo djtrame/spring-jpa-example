3cf5082e4341e0b091872bac450ad2b58f2bdaaf
djtrame <king_bucket@hotmail.com>
2024-09-02 20:26:37 -0400

    Added CORS support for an Angular page to call this service

--- src/main/java/com/trame/spring_jpa_example/SpringJpaExampleApplication.java
+++ src/main/java/com/trame/spring_jpa_example/SpringJpaExampleApplication.java
@@ -3,6 +3,9 @@ package com.trame.spring_jpa_example;
 import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @Log
@@ -10,6 +13,20 @@ public class SpringJpaExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJpaExampleApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*");
+						//.allowCredentials(true);  //throws an error... not worried about it for now
+			}
+		};
 	}
 
 }
--- src/main/java/com/trame/spring_jpa_example/controllers/StudentController.java
+++ src/main/java/com/trame/spring_jpa_example/controllers/StudentController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
@@ -25,7 +26,7 @@ public class StudentController {
         StudentEntity savedStudentEntity = studentService.save(studentEntity);
         return new ResponseEntity<>(savedStudentEntity, HttpStatus.CREATED);
     }
-
+    //@CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(path = "/students")
     public List<StudentEntity> listStudents() {
         List<StudentEntity> students = studentService.findAll();
